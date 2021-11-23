ec83651ff62d0742ffeab46535cc3825ab5d5c8f
Dalton Kirkpatrick <d.kirkpatrick99@gmail.com>
2021-11-23 17:45:42 -0600

    Updated messageBoard.jsx to functional component

--- frontend/components/messageCenter/messageBoard.jsx
+++ frontend/components/messageCenter/messageBoard.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { connect } from 'react-redux';
-// import MessageBoard from './messageBoard';
 import SideBarContainer from '../sideBar/sideBar_container'
 import BoardHeader from '../boardHeader/boardHeader'
 import ChannelShowContainer from '../channel/channel_show_container'
@@ -13,42 +12,35 @@ import { fetchAllMessages } from "../../actions/message_actions";
 // import ListenerContainer from '../channel/listener_container';
 import Modal from '../modal/modal';
 
-class MessageBoard extends React.Component {
+const MessageBoard = (props) => {
 
-    constructor(props) {
-        super(props);
-    };
-
-    componentDidMount() {
-        this.props.fetchAllUsers()
+    useEffect(() => {
+        props.fetchAllUsers()
             .then(() => {
-                this.props.fetchAllChannels();
-            //     this.props.fetchAllDirects();
-            //     this.props.fetchMemberships();
-            //     this.props.fetchAllMessages();
+                props.fetchAllChannels();
             }
-        )
+            )
         App.NotificationsChannel = App.cable.subscriptions.create(
-            { channel: "NotificationsChannel", currentUserId: this.props.currentUser },
+            { channel: "NotificationsChannel", currentUserId: props.currentUser },
             {
                 received: data => {
                     switch (data.type) {
 
                         case "membershipAdd":
-                            this.props
+                            props
                                 .receiveMembership(data.membership);
                             break;
                         case "directAdd":
-                            this.props
+                            props
                                 .fetchDirect(data.directId);
                             break;
                         case "channelAdd":
-                            this.props
+                            props
                                 .fetchChannel(data.channelId);
                             break;
                         case "userAdd":
-                            this.props.fetchUserDirects(this.props.currentUser);
-                            this.props
+                            props.fetchUserDirects(props.currentUser);
+                            props
                                 .fetchUser(data.userId);
 
                             break;
@@ -56,32 +48,33 @@ class MessageBoard extends React.Component {
                 }
             }
         );
-    }
-    componentDidUpdate() { }
+    }, [])
 
-    componentWillUnmount() {
+    useEffect(() => {
         App.NotificationsChannel.unsubscribe();
-    }
+    }) 
+    
+    // componentWillUnmount() {
+    //     App.NotificationsChannel.unsubscribe();
+    // }
 
     
 
     // houses all of the main app components and is the only route rendered while 
     // a user is logged in
-    render(){
-        return (
-            <div className="client-main-container">
-                <BoardHeader history={this.props.history} />
-                <div className="flex-container">
-                    <SideBarContainer type={this.props.type} typeId={this.props.typeId}/>
-                    <ChannelShowContainer type={this.props.type} typeId={this.props.typeId} history={this.props.history}/>
-                </div>
-                {/* <ListenerContainer type={this.props.type} typeId={this.props.typeId}/> */}
-                <Modal history={this.props.history}/>
-                
+    return (
+        <div className="client-main-container">
+            <BoardHeader history={props.history} />
+            <div className="flex-container">
+                <SideBarContainer type={props.type} typeId={props.typeId}/>
+                <ChannelShowContainer type={props.type} typeId={props.typeId} history={props.history}/>
             </div>
+            {/* <ListenerContainer type={this.props.type} typeId={this.props.typeId}/> */}
+            <Modal history={props.history}/>
+            
+        </div>
 
-        )
-    }
+    )
 
 }
 
