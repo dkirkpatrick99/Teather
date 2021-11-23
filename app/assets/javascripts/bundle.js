2bf6ed04a8cf2c0e1b6e113a78d72ca9a3e0283b
Dalton Kirkpatrick <d.kirkpatrick99@gmail.com>
2021-11-23 14:49:17 -0600

    Updated greetingHeader.jsx to functional component

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1900,91 +1900,47 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 /* harmony import */ var _actions_session_actions__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../actions/session_actions */ "./frontend/actions/session_actions.js");
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
 
 
 
 
-
-var GreetingHeader = /*#__PURE__*/function (_React$Component) {
-  _inherits(GreetingHeader, _React$Component);
-
-  var _super = _createSuper(GreetingHeader);
-
-  function GreetingHeader(props) {
-    _classCallCheck(this, GreetingHeader);
-
-    return _super.call(this, props);
-  }
-
-  _createClass(GreetingHeader, [{
-    key: "render",
-    value: function render() {
-      return (
-        /*#__PURE__*/
-        // <div className="greeting-header-container">
-        react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-          className: "header-centerizer"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-          className: "left-header-container"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-          className: "app-name-logo"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-          src: "slack-icon-logo.png",
-          alt: ""
-        }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Teather")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-          className: "dropdown"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "Past Projects"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul", {
-          className: "projects-dropdown-content"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-          href: "https://moneywise-crowdfunding.herokuapp.com/#/"
-        }, "MoneyWise")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-          href: "https://dkirkpatrick99.github.io/Super-Jelly-Hero/"
-        }, "Super Jelly Hero")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-          href: "https://ballup-app.herokuapp.com/#/"
-        }, "BallUp")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-          href: "https://github.com/dkirkpatrick99"
-        }, "GitHub"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-          href: "https://www.linkedin.com/in/dalton-kirkpatrick-9284b3184"
-        }, "LinkedIn"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-          href: "https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/"
-        }, "Portfolio")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-          className: "right-header-container"
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__.Link, {
-          to: "/login"
-        }, "Sign In"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__.Link, {
-          className: "get-started-button",
-          to: "/signup"
-        }, "Get Started"))) // </div>
-
-      );
-    }
-  }]);
-
-  return GreetingHeader;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component); // const mSTP = (state) => {
+var GreetingHeader = function GreetingHeader(props) {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "header-centerizer"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "left-header-container"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "app-name-logo"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "slack-icon-logo.png",
+    alt: ""
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Teather")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "dropdown"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "Past Projects"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul", {
+    className: "projects-dropdown-content"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://moneywise-crowdfunding.herokuapp.com/#/"
+  }, "MoneyWise")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://dkirkpatrick99.github.io/Super-Jelly-Hero/"
+  }, "Super Jelly Hero")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://ballup-app.herokuapp.com/#/"
+  }, "BallUp")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://github.com/dkirkpatrick99"
+  }, "GitHub"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://www.linkedin.com/in/dalton-kirkpatrick-9284b3184"
+  }, "LinkedIn"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/"
+  }, "Portfolio")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "right-header-container"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__.Link, {
+    to: "/login"
+  }, "Sign In"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_4__.Link, {
+    className: "get-started-button",
+    to: "/signup"
+  }, "Get Started")));
+}; // const mSTP = (state) => {
 //     return {
 //     }
 // }
--- frontend/components/greeting/greeting_header.jsx
+++ frontend/components/greeting/greeting_header.jsx
@@ -4,53 +4,44 @@ import { connect } from 'react-redux';
 import { render } from 'react-dom';
 import { signup, login } from "../../actions/session_actions"
 
-class GreetingHeader extends React.Component {
-    constructor(props) {
-        super(props);
-    };
+const GreetingHeader = (props) => {
 
-
-
-    render() {
-        return(
-            // <div className="greeting-header-container">
-                <div className='header-centerizer'>
-                    <div className="left-header-container">
-                        <div className="app-name-logo">
-                            <img src="slack-icon-logo.png" alt=""/>
-                            <h1>Teather</h1>
-                        </div>
-                        <div className="dropdown">
-                            <div>Past Projects</div>
-                            <ul className="projects-dropdown-content">
-                                <li>
-                                    <a href='https://moneywise-crowdfunding.herokuapp.com/#/'>MoneyWise</a>
-                                </li>
-                                <li>
-                                    <a href='https://dkirkpatrick99.github.io/Super-Jelly-Hero/'>Super Jelly Hero</a>
-                                </li>
-                                <li>
-                                    <a href='https://ballup-app.herokuapp.com/#/'>BallUp</a>
-                                </li>
-                            </ul>
-                        </div>
-                        
-                        <a href="https://github.com/dkirkpatrick99">GitHub</a>
-                        <a href="https://www.linkedin.com/in/dalton-kirkpatrick-9284b3184">LinkedIn</a>
-                        <a href="https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/">Portfolio</a>
-                    
-                    </div>
-
-                    <div className="right-header-container">
-                        <Link to="/login">Sign In</Link>
-                        <Link className='get-started-button' to="/signup">Get Started</Link>
-                    </div>
-
+    return(
+        <div className='header-centerizer'>
+            <div className="left-header-container">
+                <div className="app-name-logo">
+                    <img src="slack-icon-logo.png" alt=""/>
+                    <h1>Teather</h1>
                 </div>
+                <div className="dropdown">
+                    <div>Past Projects</div>
+                    <ul className="projects-dropdown-content">
+                        <li>
+                            <a href='https://moneywise-crowdfunding.herokuapp.com/#/'>MoneyWise</a>
+                        </li>
+                        <li>
+                            <a href='https://dkirkpatrick99.github.io/Super-Jelly-Hero/'>Super Jelly Hero</a>
+                        </li>
+                        <li>
+                            <a href='https://ballup-app.herokuapp.com/#/'>BallUp</a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <a href="https://github.com/dkirkpatrick99">GitHub</a>
+                <a href="https://www.linkedin.com/in/dalton-kirkpatrick-9284b3184">LinkedIn</a>
+                <a href="https://dkirkpatrick99.github.io/DaltonKirkpatrickPortfolio/">Portfolio</a>
+            
+            </div>
 
-            // </div>
-        )
-    }
+            <div className="right-header-container">
+                <Link to="/login">Sign In</Link>
+                <Link className='get-started-button' to="/signup">Get Started</Link>
+            </div>
+
+        </div>
+
+    )
 }
 
 // const mSTP = (state) => {
